1a4a7bb44e1e299606923fa11d55e0d3d412346f
MishellDallanaRoblesSerenoADSO <Mishellrobles2004@gmail.com>
2023-03-27 17:48:49 -0500

    feat: 🚀 Vue2
    avance  vuetaller2

---
+++ components/my-component.js
@@ -0,0 +1,13 @@
+export default{
+    data(){
+        return{
+            campus: [
+                {sede: 'Bucaramanga', contacto:""}, 
+                {sede: 'Bogota', contacto:""}, 
+                {sede: 'Medellin', contacto:""}, 
+                {sede: 'Mexico', contacto:""},
+                {estudiantes: {nombre: "", telefono:"", email:"", teams:{horario:"", hora:"",salones: {num:"", piso:"" }}}} 
+            ]
+        }
+    }
+}
